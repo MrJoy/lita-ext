2805af251529486bc55194846277dd7efbe4f858
Jon Frisby <jfrisby@mrjoy.com>
2015-05-09 18:29:05 -0700

    Remove some stuff that appears to be in conflict with Lita now.

--- lib/lita/ext/handler.rb
+++ lib/lita/ext/handler.rb
@@ -3,26 +3,6 @@ require 'lita/handler'
 
 module Lita
   class Handler
-    def config_valid?
-      valid = true
-      self.class.config_options.each do |config_option|
-        if config_option.required? and config[config_option.name].nil?
-          log.error "#{self.class.name.split('::').last}: missing #{config_option.name} setting"
-          valid = false
-        end
-      end
-      valid
-    end
-
-    class ConfigOption < Struct.new(
-      :name,
-      :required,
-      :type,
-      :default
-    )
-      alias_method :required?, :required
-    end
-
     class << self
       def inherited(subclass)
         handlers << subclass
@@ -31,21 +11,6 @@ module Lita
 
       def handlers
         @handlers ||= []
-      end
-
-      def config(name, required: true, type: nil, types: nil, default: nil)
-        eff_types = Array(type) + Array(types)
-        config_options << ConfigOption.new(name, required, eff_types, default)
-      end
-
-      def config_options
-        @config_options ||= []
-      end
-
-      def default_config(default)
-        config_options.each do |config_option|
-          default[config_option.name] = config_option.default
-        end
       end
     end
   end
--- spec/lita/ext/handler_spec.rb
+++ spec/lita/ext/handler_spec.rb
@@ -25,55 +25,9 @@ describe Lita::Handler, lita: true do
     expect(Lita.handlers).to include(TestHandler)
   end
 
-  describe '.config_options' do
-    it "contains specified configuration options" do
-      expect(handler_class.config_options.length).to eq(3)
-    end
-  end
-
-  describe '.config' do
-    before do
-      allow(Lita).to receive(:handlers).and_return([subject])
-      Lita::ConfigurationBuilder.load_user_config
-    end
-
-    it "defaults to required" do
-      foo_option = handler_class.config_options.select { |opt| opt.name == :foo }.first
-      expect(foo_option.required?).to eq(true)
-    end
-
-    it "required option can be used to make a config setting optional" do
-      bar_option = handler_class.config_options.select { |opt| opt.name == :bar }.first
-      expect(bar_option.required?).to eq(false)
-    end
-
-    # TODO: figure out how to initialize the handler's config object properly
-    # it "can accept a default value for the config setting" do
-    #   baz_option = handler_class.config_options.select { |opt| opt.name == :baz }.first
-    #   expect(baz_option.default).to eq("default value")
-    #   expect(subject.config[:baz]).to eq("default value")
-    # end
-  end
-
   describe '#log' do
     it "returns the Lita logger" do
       expect(subject.log).to eq(Lita.logger)
     end
-  end
-
-  describe '#config' do
-    before do
-      allow(robot).to receive(:config)#.and_return(???)
-    end
-
-    it "returns the handler's config object" do
-      fail "Need to figure out how we want to test configuration loading here."
-      # expect(subject.config).to eq(Lita.config.handlers.foo_handler)
-    end
-
-    # TODO: figure out how to initialize the handler's config object properly
-    # it "contains options specified with Lita::Handler.config" do
-    #   exepect(subject.config.baz).to eq("default value")
-    # end
   end
 end
